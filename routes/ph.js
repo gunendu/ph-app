68dd281eba41204b3f04dd9aa634a3d9717dab83
Gunendu Das <gunendudas@Gunendu.local>
2015-10-26 14:15:08 +0530

    create comment and display comment

--- routes/ph.js
+++ routes/ph.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 var UserController = require('ph-core').Controllers.UserController;
 var PostController = require('ph-core').Controllers.PostController;
+var CommentController = require('ph-core').Controllers.CommentController;
 
 router.post('/save',function(req,res,next) {
    console.log("create user is called");
@@ -53,6 +54,68 @@ router.get('/post',function(req,res,next) {
     })
     .catch(function(e) {
       console.log("get posts is called"); 
+    })  
+});
+
+router.post('/vote', function(req,res,next) {
+  console.log("vote post is called");
+  var post_id = req.param("post_id");
+  PostController.votePosts(post_id)
+    .then(function(response) {
+      return res.status(200).send({
+        "status": "success",
+        "result": response
+      }) 
+    })
+    .catch(function(e) {
+       console.log("vote posts error");  
+    })  
+});
+
+router.post('/downvote', function(req,res,next) {
+  console.log("down vote is called");
+  var post_id = req.param("post_id");
+  PostController.downvote(post_id)
+    .then(function(response) {
+      return res.status(200).send({
+        "status": "success",
+        "result": response
+      })  
+    })
+    .catch(function(e) {
+      console.log("down vote is called"); 
+    })  
+});
+
+router.post('/comment', function(req,res,next) {
+   console.log("create comment is called");
+   var post_id = req.param("post_id");
+   var comment = req.param("comment"); 
+   CommentController.create(post_id,comment)
+    .then(function(response) {
+      return res.status(200).send({
+        "status": "success",
+        "result": response
+      })  
+    })
+    .catch(function(e) {
+      console.log("create comment error");
+    })  
+});
+
+router.get('/comment/:postid', function(req,res,next) {
+  console.log("get comments is called");
+  var postid = req.params.postid;
+  console.log("postid",postid);
+  CommentController.getComments(postid)
+    .then(function(response) {
+       return res.status(200).send({
+         "status": "success",
+         "result": response 
+       })  
+    })
+    .catch(function(e) {
+      console.log("get comments error"); 
     })  
 });  
 
